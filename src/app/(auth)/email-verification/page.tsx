267a42a66cdc8824ca2774919c95a479352a34af
Philip <106172533+yankes404@users.noreply.github.com>
2024-11-17 15:02:02 +0100

    Modified verification email template

--- src/app/(auth)/email-verification/page.tsx
+++ src/app/(auth)/email-verification/page.tsx
@@ -1,14 +1,20 @@
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
-import { EmailVerificationClient } from "./client";
 import { Metadata } from "next";
+import { Suspense } from "react";
+
+import { EmailVerificationCard } from "@/features/auth/components/email-verification-card";
 
 export const metadata: Metadata = {
     title: "Email Verification - Netflix"
 }
 
-const EmailVerificationPage = async () => {
-    return <EmailVerificationClient />
+const EmailVerificationPage = () => {
+    return (
+        <Suspense>
+            <div className="w-full flex justify-center mt-8">
+                <EmailVerificationCard />
+            </div>
+        </Suspense>
+    )
 }
  
 export default EmailVerificationPage;
--- src/features/mails/components/verification-token-template.tsx
+++ src/features/mails/components/verification-token-template.tsx
@@ -1,6 +1,32 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";// Assuming you have a Text component
 import { FC } from "react";
+
+import { Html, Button, Head, Font, Body, Heading, Text, Link, Tailwind } from "@react-email/components";
+
+interface TextLinkProps {
+    href: string;
+    children: React.ReactNode;
+    target?: React.HTMLAttributeAnchorTarget;
+}
+
+export const TextLink = ({
+    href,
+    children,
+    target
+}: TextLinkProps) => {
+    return (
+        <Link
+            href={href}
+            target={target}
+            style={{
+                color: "#FF0000",
+                fontWeight: "500",
+                textDecoration: "none"
+            }}
+        >
+            {children}
+        </Link>
+    )
+}
 
 interface Props {
     name: string;
@@ -14,36 +40,75 @@ export const VerificationTokenTemplate: FC<Props> = ({
     token
 }) => {
     return (
-        <Card className="w-full min-w-[450px] max-w-[600px] shadow-lg">
-            <CardHeader>
-                <CardTitle className="text-xl font-semibold">
-                    Hi, <span className="text-red-500">{name}!</span>
-                </CardTitle>
-                <CardDescription className="text-xs">
-                    We noticed a request to verify your email for the <a href={process.env.NEXT_PUBLIC_APP_URL!} target="_blank"><span className="text-red-500 hover:text-red-400 transition font-medium">Netflix Clone</span></a> app. 
-                    If you made this request, please use the button below to confirm your email.
-                </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-                <p className="text-neutral-400 text-sm font-medium">
-                    Email: <span className="font-semibold text-red-500">{email}</span>
-                </p>
-                <Button
-                    variant="primary"
-                    size="lg"
-                    asChild
+        <Html lang="en" dir="ltr">
+            <Head>
+                <Font
+                    fontFamily="Inter"
+                    fallbackFontFamily="Arial"
+                />
+            </Head>
+            <Body>
+                <div
+                    style={{
+                        width: "450px",
+                        maxWidth: "100%",
+                        padding: "20px",
+                        border: "1px solid hsl(0, 0%, 89.8%)",
+                        borderRadius: "8px",
+                        boxShadow: "0 4px 8px 0 hsla(0, 0%, 0%, 0.1)"
+                    }}
                 >
-                    <a
+                    <Heading
+                        as="h1"
+                        style={{
+                            padding: "0",
+                            margin: "0",
+                            fontSize: "18px"
+                        }}
+                    >
+                        Hi, <span style={{ color: "#FF0000" }}>{name}!</span>
+                    </Heading>
+                    <Text
+                        style={{
+                            fontSize: "12px",
+                            fontWeight: "500",
+                            marginTop: "4px",
+                            color: "hsl(0, 0%, 45.1%)",
+                            lineHeight: "20px"
+                        }}
+                    >
+                        We noticed a request to verify your email for the&nbsp;<TextLink href={process.env.NEXT_PUBLIC_APP_URL!} target="_blank">Netflix Clone</TextLink>&nbsp;app. If you made this request, please use the button below to confirm your email.
+                    </Text>
+                    <Link
                         href={`${process.env.NEXT_PUBLIC_APP_URL}/email-verification?token=${token}`}
                         target="_blank"
+                        style={{
+                            background: "#FF0000",
+                            color: "#FFF",
+                            padding: "8px",
+                            borderRadius: "6px",
+                            fontWeight: "600",
+                            fontSize: "11px",
+                            width: "140px",
+                            display: "block",
+                            textAlign: "center",
+                            boxShadow: "0 4px 8px 0 hsla(0, 0%, 0%, 0.1)"
+                        }}
                     >
                         Verify Email Address
-                    </a>
-                </Button>
-            </CardContent>
-            <CardFooter className="text-xs font-medium text-neutral-400">
-                If you did not make this request, please ignore this email.
-            </CardFooter>
-        </Card>
-    );
+                    </Link>
+                    <footer
+                        style={{
+                            color: "hsl(0, 0%, 45.1%)",
+                            fontSize: "11px",
+                            marginTop: "16px",
+                            fontWeight: "500"
+                        }}
+                    >
+                        If you did not make this request, please ignore this email.
+                    </footer>
+                </div>
+            </Body>
+        </Html>
+    )
 };
